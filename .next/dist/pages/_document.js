e4e7100131dc338932f2d35510d3032a52b63e54
Mustansir Zia <mustansir29@hotmail.com>
2017-11-07 01:22:27 +0530

    Genesis.

---
+++ .next/dist/pages/_document.js
@@ -0,0 +1,4 @@
+'use strict';
+
+module.exports = require('next/dist/server/document.js');
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm5vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvcGFnZXMvX2RvY3VtZW50LmpzIl0sIm5hbWVzIjpbIm1vZHVsZSIsImV4cG9ydHMiLCJyZXF1aXJlIl0sIm1hcHBpbmdzIjoiOztBQUFBLE9BQU8sQUFBUCxVQUFpQixBQUFqQiIsImZpbGUiOiJfZG9jdW1lbnQuanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL011c3RhbnNpci9Xb3Jrc3BhY2UvR3JhcGhpY1dlYXZlL0FyZWViL0Rhc2hib2FyZCJ9
---
+++ .next/dist/pages/index.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJpbmRleC5qcz9lbnRyeSIsInNvdXJjZVJvb3QiOiIvVXNlcnMvTXVzdGFuc2lyL1dvcmtzcGFjZS9HcmFwaGljV2VhdmUvQXJlZWIvRGFzaGJvYXJkIn0=
